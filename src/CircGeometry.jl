c46bcb9809e4984b0e039daefa3fa20be11fd0e8
Patrick Eastham <peastham@math.fsu.edu>
2020-04-22 14:01:11 -0400

    started expanding code

--- src/CircGeometry.jl
+++ src/CircGeometry.jl
@@ -1,5 +1,22 @@
 module CircGeometry
 
-greet() = print("Hello World!")
+abstract type AbstractPlacementObject end
+abstract type AbstractCircle end
+
+mutable struct Point{T}
+    x::T
+    y::T
+end
+
+struct PlacementCircle{T} <: AbstractPlacementObject
+    radius::T
+    center::Point{T}
+end
+
+struct MaterialParameters{T}
+    outer_buffer_percent::T
+    between_buffer_percent::T
+end
+
 
 end # module
